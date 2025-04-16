3e07e3c8133a191c311fbde48570349971e6fdea
Akim Kulikouski <qlikovsky@gmail.com>
2025-04-16 18:51:22 +0300

    feat: add routing and cart 404 logic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,13 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter } from 'react-router';
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-	// <StrictMode>
+	<StrictMode>
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<App />} />
-			</Routes>
+			<App />
 		</BrowserRouter>
-	// </StrictMode>
+	</StrictMode>
 );
